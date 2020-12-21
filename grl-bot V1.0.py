a83146a0815410529a38261667a7365e3ec8728c
SauCoolerProgrammierer <chaingame32@gmail.com>
2020-12-21 19:29:36 +0100

    Command to insult a grl member

--- grl-bot V1.0.py
+++ grl-bot V1.0.py
@@ -1,18 +1,18 @@
-import os
-import discord
-from discord.ext import commands
 import random
-import requests
 from time import sleep
 
-#weird new Intents
-#not sure if they are all necessary
+import discord
+import requests
+from discord.ext import commands
+
+# weird new Intents
+# not sure if they are all necessary
 intents = discord.Intents()
 intents.members = True
 intents.guilds = True
 intents.messages = True
 
-bot = commands.Bot(command_prefix = '.', intents = intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 #startup message in console
 @bot.event
@@ -39,7 +39,7 @@ async def test(ctx, name):
 @bot.command(name='memes', help='Get popular memes')
 async def memes(ctx, number):
     url = 'https://meme-api.herokuapp.com/gimme'
-    
+
     if int(number) > 5:
         await ctx.send('Please don\'t ask for that many memes')
     else:
@@ -48,7 +48,14 @@ async def memes(ctx, number):
             data = response.json()
             await ctx.send('{}'.format(data['url']))
             sleep(1)
-    
+
+
+# insults a grl member
+@bot.command(name='insult', help='Insult a grl member')
+async def insult(ctx, name):
+    insults = open('insults.txt').read().splitlines()
+    await ctx.send(f'{name} you {random.choice(insults)}')
+
 
 f = open('token.txt', 'r')
 token = f.read()
