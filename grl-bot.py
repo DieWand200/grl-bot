c41b7efa186181410e5f7d7abbb9c8a51e6c1d7f
DieWand <wand200.nd@gmail.com>
2020-12-22 23:48:53 +0100

    Update grl-bot.py
    executable path removed due to change to linux

--- grl-bot.py
+++ grl-bot.py
@@ -98,7 +98,7 @@ async def insultvc(context, name):
         # connect to vc und play audio
         vc = context.message.author.voice.channel
         currentvc = await vc.connect()
-        currentvc.play(discord.FFmpegPCMAudio(filename, executable='C:/Users/wand2/ffmpeg/bin/ffmpeg.exe', options="-loglevel panic"))
+        currentvc.play(discord.FFmpegPCMAudio(filename, options="-loglevel panic"))
         while currentvc.is_playing():
             await asyncio.sleep(1)
         # disconnect after the player has finished
