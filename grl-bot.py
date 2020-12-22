853f06d15b5f623fb9937acd3703ac7c887640b1
DieWand <wand200.nd@gmail.com>
2020-12-22 18:27:52 +0100

    Update grl-bot.py
    nope didnt work, next try

--- grl-bot.py
+++ grl-bot.py
@@ -70,18 +70,18 @@ async def insultvc(context, name):
         # text to speech and save as mp3
         insults = open('insults.txt').read().splitlines()
         audio = gTTS(text=f'{name.replace("@", "")} you {random.choice(insults)}', lang='en', slow=False)
-        filename = 'insult-' + str(time.time()) + '.mp3'
-        audio.save(/grl-bot/filename)
+        filename = '/grl-bot/insult-' + str(time.time()) + '.mp3'
+        audio.save(filename)
 
         # connect to vc und play audio
         vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(/grl-bot/filename))
+        vc.play(discord.FFmpegPCMAudio(filename))
         # Sleep while audio is playing.
         while vc.is_playing():
             sleep(.5)
         # disconnect from vc and delete mp3 file
         await vc.disconnect()
-        os.remove(/grl-bot/filename)
+        os.remove(filename)
     else:
         await context.send(f'Please connect to a voice channel first. You {insult}')
 
